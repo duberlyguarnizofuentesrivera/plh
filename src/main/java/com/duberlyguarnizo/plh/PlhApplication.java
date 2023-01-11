3dbc0cc5f6ac7aad7a0f51254ba69c6296afe6ce
Duberly Guarnizo <duberlygfr@gmail.com>
2023-01-11 11:00:58 -0500

    Initial commit

---
+++ src/main/java/com/duberlyguarnizo/plh/PlhApplication.java
@@ -0,0 +1,10 @@
+package com.duberlyguarnizo.plh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlhApplication.class, args
---
+++ src/test/java/com/duberlyguarnizo/plh/PlhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.plh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
